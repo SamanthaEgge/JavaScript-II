fe820fb49e42d0010bbbc7f15bd5fc8d4d8387e3
unknown <samantha.c.egge@gmail.com>
2019-05-07 16:59:29 -0700

    Some additional stretch goal work and fiddling with counters in closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -20,12 +20,32 @@ smiteGame(0, 1);
 
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
-};
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+
+const counter = () => {
+  let count = 0;
+  return function () {
+    count = count+1;
+    return count;
+  }
+}
+
+let newCounter = counter();
+newCounter();
+newCounter();
+newCounter();
+
+counter();
+counter();
+counter();
+
+console.log(counter());
+console.log(counter()());
+console.log(newCounter());
+
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
